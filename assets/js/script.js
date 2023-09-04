4d746afaaec05427851ce4e87e7ec9849b539df3
AzizLike29 <abdulfirdaus590@gmail.com>
2023-09-04 12:07:50 +0700

    Menambah sintaks javascript dan mengubah id di html

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,28 @@ const nav = document.querySelector(".navbar ul");
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
+
+// Membuat DOM selection
+document.getElementById("submitButton").addEventListener("click", function () {
+  // Ambil nilai dari input form
+  const name = document.getElementById("inputName").value;
+  const role = document.getElementById("inputRole").value;
+  const availability = document.getElementById("inputAvailability").value;
+  const age = document.getElementById("inputAge").value;
+  const location = document.getElementById("inputLocation").value;
+  const experience = document.getElementById("inputExperience").value;
+  const email = document.getElementById("inputEmail").value;
+
+  // Ganti nilai dalam elemen-elemen profil dengan nilai dari form
+  document.getElementById("profilName").textContent = name;
+  document.getElementById("profileRole").textContent = role;
+  document.getElementById(
+    "availability"
+  ).textContent = `Availability: ${availability}`;
+  document.getElementById("age").textContent = `Age: ${age}`;
+  document.getElementById("location").textContent = `Location: ${location}`;
+  document.getElementById(
+    "experience"
+  ).textContent = `Experience: ${experience} Tahun`;
+  document.getElementById("email").textContent = `Email: ${email}`;
+});
